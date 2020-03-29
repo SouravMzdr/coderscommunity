c28e1d45c12c83f463cfa56dc569d2360feb507a
sourav <souravmzdr@gmail.com>
2020-03-29 15:36:20 +0530

    acess

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const path = require('path');
 const compression = require('compression')
+var cors = require('cors');
+
 const app = express();
 app.use(compression())
+app.use(cors({origin:true}));
+
+// app.use(cors());
+
+ app.use(function(req, res, next) {
+
+     res.setHeader("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+     res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+     res.setHeader("Access-Control-Allow-Methods","*");
+     next();
+   });
+
 
 // Serve static files....
 app.use(express.static(__dirname + '/dist/QandA'));
